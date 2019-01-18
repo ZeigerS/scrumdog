5773d91e6b1f807e700d904831f1f28e3a25902a
mugatu <mugatu@mugatu-K46CA>
2019-01-18 09:56:48 -0500

    save my stuff

---
+++ Project_3/src/aura/DiagnosisDiagnosisItem/DiagnosisDiagnosisItemController.js
@@ -0,0 +1,7 @@
+({
+	myClickEvent : function(component, event, helper) {
+		helper.myClickEvent(component, event);
+		
+		var modalBool = component.get("v.trueToModal");
+	}
+})
---
+++ Project_3/src/aura/DiagnosisDiagnosisItem/DiagnosisDiagnosisItemHelper.js
@@ -0,0 +1,8 @@
+({
+	myClickEvent : function(component, event) {
+	
+		var modalBool = component.get("v.trueToModal");
+		
+		
+	}
+})
